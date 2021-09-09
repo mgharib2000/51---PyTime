f727db35121cd32dec96d72364e76e94a6e6c705
Mohamed <83606701+mgharib2000@users.noreply.github.com>
2021-09-09 19:21:04 +0100

    Update 51 - PyTime.py

--- 51 - PyTime.py
+++ 51 - PyTime.py
@@ -1,4 +1,3 @@
-  
 import tkinter as tk
 from tkinter import *
 from time import strftime
@@ -18,13 +17,14 @@ app.rowconfigure(0, weight=1)
 app.rowconfigure(1, weight=1)
 app.rowconfigure(2, weight=1)
 
-clock_label = Label(app, bg="black", fg="white", font = ("Times", 40, "bold"), relief=GROOVE)
-clock_label.grid(row=1, column=1)
+clock_label = Label(app, bg="black", fg="white", font = ("Times", 28, "bold"), relief=GROOVE)
+clock_label.grid(row=1, column=1, sticky=EW)
 
 def update_label():
-    current_time = strftime('%H: %M: %S')
+    current_time = strftime("%I: %M: %S %p")
     clock_label.configure(text = current_time)
-    clock_label.after(80, update_label)
+    clock_label.after(1000, update_label)
 
 update_label()
 app.mainloop()
+
